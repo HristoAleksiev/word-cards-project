00ad0739ef61cbcaad9bf4bc790857ede4a63be4
Алексиев <kadoll555@gmail.com>
2021-05-13 01:09:31 +0300

    added save progress functionality into a new csv file

--- main.py
+++ main.py
@@ -8,29 +8,47 @@ window = Tk()
 window.title("Language Flash Cards")
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
-# Reads from the csv file and loads all the words into a list of dictionaries:
-words_cvs_file_data = pandas.read_csv("data/french_words.csv")
-words_dict = words_cvs_file_data.to_dict(orient="records")
 
-
-def flip_card(word):
+def flip_card():
     canvas.itemconfig(card_side, image=canvas_image_back)
     canvas.itemconfig(language, text="English", fill="white")
-    canvas.itemconfig(current_word, text=word["English"], fill="white")
+    canvas.itemconfig(current_word, text=next_word["English"], fill="white")
 
 
 def get_next_word():
-    canvas.itemconfig(card_side, image=canvas_image_front)
+    global next_word, timer
+    window.after_cancel(timer)
 
     next_word = words_dict[r.randint(0, len(words_dict) - 1)]
     print(next_word)
 
+    canvas.itemconfig(card_side, image=canvas_image_front)
     canvas.itemconfig(language, text="French", fill="black")
     canvas.itemconfig(current_word, text=next_word["French"], fill="black")
     canvas.update()
 
-    window.after(3000, flip_card(next_word))
+    timer = window.after(3000, flip_card)
 
+
+def word_is_known():
+    words_dict.remove(next_word)
+    unknown_known_words = pandas.DataFrame(words_dict)
+    unknown_known_words.to_csv("data/words_to_learn.csv", index=False)
+    print(len(words_dict))
+
+    get_next_word()
+
+
+# Reads from the csv file and loads all the words into a list of dictionaries:
+try:
+    words_cvs_file_data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    words_cvs_file_data = pandas.read_csv("data/french_words.csv")
+    words_dict = words_cvs_file_data.to_dict(orient="records")
+    next_word = words_dict[r.randint(0, len(words_dict) - 1)]
+else:
+    words_dict = words_cvs_file_data.to_dict(orient="records")
+    next_word = words_dict[r.randint(0, len(words_dict) - 1)]
 
 # Loading all the pictures to be used in the program:
 canvas_image_front = PhotoImage(file="images/card_front.png")
@@ -47,10 +65,12 @@ current_word = canvas.create_text(400, 264, text="trouve", font=("Ariel", 60, "b
 
 canvas.grid(column=0, row=0, columnspan=2)
 
+timer = window.after(3000, func=flip_card)
+
 button_wrong = Button(image=button_img_wrong, highlightthickness=0, bd=0, command=get_next_word)
 button_wrong.grid(column=0, row=1)
 
-button_right = Button(image=button_img_right, highlightthickness=0, bd=0, command=get_next_word)
+button_right = Button(image=button_img_right, highlightthickness=0, bd=0, command=word_is_known)
 button_right.grid(column=1, row=1)
 
 get_next_word()
